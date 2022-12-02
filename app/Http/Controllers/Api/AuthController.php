12bb85c9d031426b25b203b64c2bd2756128224e
chungisugithub <chungisu@gmail.com>
2022-12-03 00:46:39 +0900

    사용자등록 signup()

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
@@ -11,8 +12,24 @@ class AuthController extends Controller
         echo "Login Endpoint Requested";
     }
 
-    public function signup() {
-        echo "Signup Endpoint Requested";
+    public function signup(Request $request) {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+
+        $user = new User([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            'message' => "User registered successfully"
+        ], 201);
     }
 
     public function index() {
